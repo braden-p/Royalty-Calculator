7d164b4323c2766f04ae6756d1b45d00ced8ad2a
Braden Piper <bgpiper@gmail.com>
2023-08-23 17:18:03 -0500

    Achieve first working version of Royalty_Calculator - physical only
    Resolve keyErrors in code, fix net-rate calculation, fix penny-rate handling, add item to TODO, create first royalty-run spreadsheet

--- Royalty_Calculator.py
+++ Royalty_Calculator.py
@@ -15,21 +15,39 @@ licenses_df = pd.read_excel('Licenses.xlsx')
 sales_df = pd.read_excel('Physical_Sales.xlsx')
 
 # Filter Relevant Columns
-sales_filtered = sales_df[['product-type', 'upc-ean', 'net-units']]
-licenses_filtered = licenses_df[['upc', 'product-type', 'publisher', 'album-title', 'catalog-no', 'track-title', 'ISRC', 'share', 'rate-type', 'rate-percent', 'track-minutes', 'track-seconds']]
+sales_filtered = sales_df[['product-type', 'upc', 'net-units']]
+licenses_filtered = licenses_df[['upc', 'product-type', 'publisher', 'admin', 'agent', 'album-title', 'catalog-no', 'track-title', 'isrc', 'share', 'rate-type', 'rate-percent', 'track-minutes', 'track-seconds', 'penny-rate']]
 
 # Merge Data and Perform Calculations
 merged_df = pd.merge(sales_filtered, licenses_filtered, how='inner', on=['upc', 'product-type'])
 
-# Calculate song length in seconds
-merged_df['song-length'] = merged_df['track-minutes'] * 60 + merged_df['track-seconds']
+# Calculate net rate based on rate-type
+def calculate_net_rate(row):
+    if row['rate-type'] == 'Penny Rate':
+        return row['penny-rate']
+    elif row['rate-type'] == 'Full Stat':
+        if row['track-minutes'] < 5:
+            return 0.091
+        elif row['track-minutes'] == 5 and row['track-seconds'] == 0:
+            return 0.091
+        else:
+            return (row['track-minutes'] + 1) * 0.0175
+    elif row['rate-type'] == 'Min Stat':
+        return 0.091
+    else:
+        return None  # Handle unknown rate-types if needed
 
-# Calculate net rate based on rate-type and rate-percent
-merged_df['net-rate'] = merged_df.apply(lambda row: row['rate-percent'] if row['rate-type'] == 'percentage' else row['penny-rate'], axis=1)
+merged_df['net-rate'] = merged_df.apply(calculate_net_rate, axis=1)
 
 # Calculate balance
-merged_df['balance'] = merged_df['share'] * merged_df['net-rate'] * merged_df['net-units']
+def calculate_balance(row):
+    if row['rate-type'] == 'Penny Rate':
+        return row['penny-rate'] * row['net-units']
+    else:
+        return (row['share'] / 100) * row['net-rate'] * row['net-units']
+
+merged_df['balance'] = merged_df.apply(calculate_balance, axis=1)
 
 # Create New Excel Spreadsheet
-royalty_run_df = merged_df[['publisher', 'album-title', 'catalog-no', 'upc', 'track-title', 'ISRC', 'product-type', 'share', 'net-rate', 'net-units', 'balance']]
+royalty_run_df = merged_df[['publisher', 'admin', 'agent', 'album-title', 'catalog-no', 'upc', 'track-title', 'isrc', 'product-type', 'share', 'net-rate', 'net-units', 'balance']]
 royalty_run_df.to_excel('royalty-run.xlsx', index=False)
