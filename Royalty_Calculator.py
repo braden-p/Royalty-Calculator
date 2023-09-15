ce7a81a07ac8caafa82bbb0e2c169ee8fa6aa54a
Braden Piper <bgpiper@gmail.com>
2023-09-14 23:16:34 -0500

    Add Digital Album Sales File Processing

--- Royalty_Calculator.py
+++ Royalty_Calculator.py
@@ -12,14 +12,34 @@ import pandas as pd
 
 # Import Excel Files
 licenses_df = pd.read_excel('Licenses.xlsx')
-sales_df = pd.read_excel('Physical_Sales.xlsx')
+physical_sales_df = pd.read_excel('Physical_Sales.xlsx')
+digital_album_sales_df = pd.read_excel('Digital_Album_Sales.xlsx')
+
+# Define a mapping dictionary for product types
+product_type_mapping = {
+    'DA': 'All Digital',
+    'DT': 'All Digital'
+}
+
+# Create a new column 'merged-product-type' in Digital Sales DataFrames using the mapping
+digital_album_sales_df['merged-product-type'] = digital_album_sales_df['product-type'].map(product_type_mapping)
+#digital_tracks_sales_df['merged-product-type'] = digital_tracks_sales_df['product-type'].map(product_type_mapping)
+
+# Add a new column 'original-product-type' to Digital Sales DataFrames to store the original product type
+digital_album_sales_df['original-product-type'] = digital_album_sales_df['product-type']
+#digital_tracks_sales_df['original-product-type'] = digital_tracks_sales_df['product-type']
 
 # Filter Relevant Columns
-sales_filtered = sales_df[['product-type', 'upc', 'net-units']]
+physical_sales_filtered = physical_sales_df[['product-type', 'upc', 'net-units']]
+digital_album_sales_filtered = digital_album_sales_df[['merged-product-type', 'original-product-type', 'upc', 'net-units']]
+#digital_tracks_sales_filtered = digital_tracks_sales_df[['merged-product-type', 'original-product-type', 'upc', 'net-units']]
 licenses_filtered = licenses_df[['upc', 'product-type', 'publisher', 'admin', 'agent', 'album-title', 'catalog-no', 'track-number', 'track-title', 'isrc', 'share', 'rate-type', 'rate-percent', 'track-minutes', 'track-seconds', 'lock-date', 'penny-rate']]
 
+# Combine Physical and Digital Sales DataFrames
+combined_sales_df = pd.concat([physical_sales_filtered, digital_album_sales_filtered], ignore_index=True)
+
 # Merge Data and Perform Calculations
-merged_df = pd.merge(sales_filtered, licenses_filtered, how='inner', on=['upc', 'product-type'])
+merged_df = pd.merge(combined_sales_df, licenses_filtered, how='inner', left_on=['upc', 'merged-product-type'], right_on=['upc', 'product-type'])
 
 # Calculate rate based on lock-date and rate-type
 def calculate_rate(row):
@@ -148,5 +168,6 @@ def calculate_balance(row):
 merged_df['balance'] = merged_df.apply(calculate_balance, axis=1)
 
 # Create New Excel Spreadsheet
-royalty_run_df = merged_df[['publisher', 'admin', 'agent', 'album-title', 'catalog-no', 'upc', 'track-number', 'track-title', 'isrc', 'product-type', 'rate-period', 'share', 'net-rate', 'net-units', 'balance']]
+royalty_run_df = merged_df[['publisher', 'admin', 'agent', 'album-title', 'catalog-no', 'upc', 'track-number', 'track-title', 'isrc', 'original-product-type', 'merged-product-type', 'rate-period', 'share', 'net-rate', 'net-units', 'balance']]
 royalty_run_df.to_excel('royalty-run.xlsx', index=False)
+
