ba18aebcb7438dcc6624e98b478d6868b37d0302
Braden Piper <bgpiper@gmail.com>
2023-10-26 15:08:56 -0500

    Add digital track processing to main royalty-calculator file and combine
    Royalty-Calculator.py now runs all three separate royalty-calculator processes: one for physical sales, one for digital album sales, one for digital track sales, and combines the results into one file.

--- Royalty_Calculator.py
+++ Royalty_Calculator.py
@@ -8,8 +8,25 @@ DESCRIPTION:
 A mechanical royalty calculation and management software.
 """
 
+# Process Physical Sales Royalty Run
 with open("Physical_Sales_Royalty_Calculator.py") as process_physical:
     exec(process_physical.read())
 
+# Process Digital Album Sales Royalty Run
 with open("Digital_Album_Sales_Royalty_Calculator.py") as process_digital_albums:
     exec(process_digital_albums.read())
+
+# Process Digital Track Sales Royalty Run
+with open("Digital_Track_Sales_Royalty_Calculator.py") as process_digital_tracks:
+    exec(process_digital_tracks.read())
+
+# Load the three ouput Excel files into separate DataFrames
+df_digital_albums = pd.read_excel('royalty-run-digital-albums.xlsx')
+df_digital_tracks = pd.read_excel('royalty-run-digital-tracks.xlsx')
+df_physical = pd.read_excel('royalty-run-physical.xlsx')
+
+# Concatenate the DataFrames vertically
+result = pd.concat([df_digital_albums, df_digital_tracks, df_physical])
+
+# Save the merged DataFrame to a new Excel file
+result.to_excel('royalty-run.xlsx', index=False)
